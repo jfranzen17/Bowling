c63e08a4b968b10bda5fd41fc37c5d9bba224818
Johan <jfranzen17@gmail.com>
2021-05-26 14:44:21 +0200

    fixed

---
+++ BowlingRound/ClassLibrary.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Player
+    {
+        public string name;
+        public int score;
+    }
+
+    public class BowlingWithPeople
+
+    {
+        public List<Player> PlayerList = new List<Player>();
+        public Player CurrentPlayer;
+
+        public List<Player> StartGame(int numberOfPlayers)
+        {
+            var listOfPlayers = PlayerList;
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                var newPerson = new Player();
+
+                if (i == 0)
+                {
+                    newPerson.name = "Juan";
+                    CurrentPlayer = newPerson;
+                }
+
+                if (i == 1)
+                    newPerson.name = "Mason";
+
+                if (i == 2)
+                    newPerson.name = "Alex";
+
+                if (i == 3)
+                    newPerson.name = "Duncan";
+
+                listOfPlayers.Add(newPerson);
+            }
+
+            return listOfPlayers;
+        }
+
+        public void PrintPlayerNamesAndScores(List<Player> PlayerList)
+
+        {
+            foreach (var person in PlayerList)
+
+            {
+                Console.WriteLine("Name of player: " + person.name + " Score of player: " + person.score);
+            }
+        }
+    }
+
+
+    public class BowlingGame
+    {
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
+        }
+    }
+}
---
+++ BowlingRoundTest/BowlingTest.cs
@@ -0,0 +1,197 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+
+namespace BowlingRoundTest
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        private BowlingGame bowlingGame;
+
+        public BowlingGameTest()
+        {
+            bowlingGame = new BowlingGame();
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                bowlingGame.Roll(pins);
+            }
+        }
+
+        [TestMethod]
+        public void Roll_AllGutters_ScoreZero()
+        {
+            RollMany(20, 0);
+            Assert.AreEqual(0, bowlingGame.Score());
+        }
+        [TestMethod]
+        public void RollReturnsTheNumberOfPinsKnockedDown()
+        {
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(2); ;
+            Assert.AreEqual(6, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void Roll_AllTwos_Score40()
+        {
+            RollMany(20, 2);
+            Assert.AreEqual(40, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void CanRoll_Spare_Score14()
+        {
+            bowlingGame.Roll(6);
+            bowlingGame.Roll(4);
+            bowlingGame.Roll(2);
+
+            Assert.AreEqual(14, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void CanRoll_Strike_Score26()
+        {
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(2);
+            bowlingGame.Roll(6);
+
+            Assert.AreEqual(26, bowlingGame.Score());
+        }
+        [TestMethod]
+        public void CanRoll_MaxGame_Score300()
+        {
+            RollMany(12, 10);
+            Assert.AreEqual(300, bowlingGame.Score());
+        }
+        [TestMethod]
+        public void CanRoll_AllSpare_Score150()
+        {
+            RollMany(21, 5);
+            Assert.AreEqual(150, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void Roll_ASpareThenAStrike_Score38()
+        {
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(10);
+            bowlingGame.Roll(2);
+            bowlingGame.Roll(2);
+            Assert.AreEqual(38, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void Roll_ExtraRollIfSpareOrStrikeInLastFrame()
+        {
+            RollMany(18, 2);
+
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(5);
+            bowlingGame.Roll(2);
+            Assert.AreEqual(48, bowlingGame.Score());
+
+        }
+
+        [TestMethod]
+        public void Roll_DontGetExtraFrame()
+        {
+            RollMany(21, 2);
+            Assert.AreEqual(40, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void RollAFullGame_TestRandomGameNoExtraRoll_Returns113()
+        {
+            bowlingGame.Roll(new int[] { 2, 8, 3, 7, 8, 1, 10, 6, 1, 5, 2, 5, 1, 8, 0, 10, 9, 0 });
+            Assert.AreEqual(113, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void Score_Roll_TestRandomGameWithSpareThenStrikeAtEnd_Returns120()
+        {
+            bowlingGame.Roll(new int[] { 1, 1, 2, 3, 5, 5, 10, 10, 7, 2, 6, 2, 1, 3, 4, 2, 8, 2, 10});
+            Assert.AreEqual(120, bowlingGame.Score());
+        }
+
+        [TestMethod]
+        public void Score_Roll_TestRandomGameWithThreeStrikesAtEnd_Returns160()
+        {
+            bowlingGame.Roll(new int[] { 5, 5, 4, 2, 7, 3, 10, 9, 1, 5, 4, 10, 4, 4, 8, 2, 10, 10, 10});
+            Assert.AreEqual(160, bowlingGame.Score());
+
+        }
+
+        [TestMethod]
+        public void Game_StartAGameWith4People_ReturnTrue()
+        {
+
+            var newGame = new BowlingWithPeople();
+
+            newGame.StartGame(2);
+
+            var actual = newGame.PlayerList.Count;
+
+            Assert.AreEqual(actual, 2);
+
+        }
+
+        [TestMethod]
+        public void Game_ActivePlayer_ReturnTrue()
+        {
+            var newGame = new BowlingWithPeople();
+
+            var newPlayer = new Player();
+            newGame.PlayerList.Add(newPlayer);
+
+            newGame.CurrentPlayer = newPlayer;
+
+            var actual = newPlayer;
+
+            Assert.AreEqual(actual, newGame.CurrentPlayer);
+
+        }
+
+        [TestMethod]
+        public void Game_GetSecondPlayersScore_Returns7()
+        {
+
+            var newGame = new BowlingWithPeople();
+
+            var listOfPlayers = newGame.StartGame(2);
+
+            bowlingGame.Roll(7);
+            bowlingGame.Roll(2);
+            int score1 = bowlingGame.Score();
+            newGame.CurrentPlayer.score = score1;
+
+            newGame.CurrentPlayer = listOfPlayers[1];
+
+            bowlingGame.Roll(1);
+            bowlingGame.Roll(6);
+            int score2 = bowlingGame.Score() - score1;
+            newGame.CurrentPlayer.score = score2;
+
+            var actual = listOfPlayers[1].score;
+
+            Assert.AreEqual(7, actual);
+        }
+
+        [TestMethod]
+        public void Game_GetNameFromPlayer_ReturnTrue()
+        {
+            var newGame = new BowlingWithPeople();
+
+            var ListOfPlayers = newGame.StartGame(2);
+
+            var actual = ListOfPlayers[1].name;
+
+            Assert.AreEqual("Mason", actual);
+        }
+    }
+}
